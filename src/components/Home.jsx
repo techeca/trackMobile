0211d8a21416540dbef84f32540429aec0c5ca9d
techeca <jp.vasquez@outlook.com>
2024-12-21 19:04:24 -0300

    up

---
+++ src/components/Home.jsx
@@ -0,0 +1,114 @@
+export default function Home() {
+
+    return (
+        <div className='flex flex-col gap-3 border-0 rounded-3xl w-96 items-center h-[812px] bg-nig-80 overflow-hidden'>
+            <div className="bg-nig-70/50 h-[430px] w-full rounded-b-4xl flex flex-col items-center">
+                <div className="flex w-full justify-end pr-6 mt-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 24 24"><g fill="none"><circle cx={12} cy={12} r={2} stroke="currentColor"></circle><path fill="currentColor" d="m5.399 5.88l.25-.434a.5.5 0 0 0-.617.093zM3.4 9.344l-.478-.148a.5.5 0 0 0 .228.58zm-.002 5.311l-.25-.433a.5.5 0 0 0-.228.581zm2 3.464l-.367.34a.5.5 0 0 0 .617.093zm4.6 2.655h-.5a.5.5 0 0 0 .39.488zm4.001.002l.111.488a.5.5 0 0 0 .389-.488zM18.6 18.12l-.25.433a.5.5 0 0 0 .617-.093zm1.998-3.466l.478.148a.5.5 0 0 0-.228-.58zm.002-5.311l.25.433a.5.5 0 0 0 .228-.581zm-2-3.465l.367-.34a.5.5 0 0 0-.617-.093zM14 3.225h.5a.5.5 0 0 0-.389-.487zm-4-.002l-.111-.488a.5.5 0 0 0-.39.488zm4 1.849h-.5zm5 8.66l-.25.433zm-2 3.464l-.25.433zM5 13.732l.25.433zm2-6.928l-.25.433zM3.878 9.492a8.5 8.5 0 0 1 1.887-3.273l-.733-.68a9.5 9.5 0 0 0-2.11 3.658zm.76 6.758a8.5 8.5 0 0 1-.761-1.742l-.956.296a9.5 9.5 0 0 0 .852 1.946zm1.128 1.53a8.5 8.5 0 0 1-1.127-1.53l-.866.5a9.5 9.5 0 0 0 1.259 1.71zm8.123 2.51a8.5 8.5 0 0 1-3.778-.002l-.222.974a9.5 9.5 0 0 0 4.222.003zm6.233-5.782a8.5 8.5 0 0 1-1.887 3.273l.733.68a9.5 9.5 0 0 0 2.11-3.658zm-.76-6.758c.324.563.577 1.147.762 1.742l.955-.296a9.5 9.5 0 0 0-.852-1.946zm-1.128-1.53a8.5 8.5 0 0 1 1.127 1.53l.866-.5a9.5 9.5 0 0 0-1.259-1.71zm-8.123-2.51a8.5 8.5 0 0 1 3.778.002l.222-.974a9.5 9.5 0 0 0-4.222-.003zm.389 1.362v-1.85h-1v1.85zM7.25 6.37l-1.601-.925l-.5.866l1.6.925zm-2.5 6.928l-1.601.924l.5.866l1.6-.924zm.5-3.464l-1.6-.923l-.5.866l1.6.923zm5.25 10.94v-1.847h-1v1.847zm-3.75-4.012l-1.601.924l.5.866l1.6-.924zm12.101.925l-1.601-.925l-.5.866l1.601.925zm-4.351 3.09v-1.85h-1v1.85zM20.351 8.91l-1.601.924l.5.866l1.601-.924zm.498 5.311L19.25 13.3l-.5.866l1.6.923zM14.5 5.072V3.225h-1v1.847zm3.851.374l-1.601.925l.5.866l1.601-.925zM13.5 5.072c0 1.924 2.083 3.127 3.75 2.165l-.5-.866a1.5 1.5 0 0 1-2.25-1.3zm5.25 4.763c-1.667.962-1.667 3.368 0 4.33l.5-.866a1.5 1.5 0 0 1 0-2.598zm-1.5 6.928c-1.667-.962-3.75.24-3.75 2.165h1a1.5 1.5 0 0 1 2.25-1.299zm-6.75 2.165c0-1.924-2.083-3.127-3.75-2.165l.5.866a1.5 1.5 0 0 1 2.25 1.3zm-5.25-4.763c1.667-.962 1.667-3.368 0-4.33l-.5.866c1 .577 1 2.02 0 2.598zM9.5 5.072A1.5 1.5 0 0 1 7.25 6.37l-.5.866c1.667.962 3.75-.24 3.75-2.165z"></path></g></svg>
+                </div>
+
+                <div className="w-[286px] h-[286px] border-2">
+
+                </div>
+                <div className="flex gap-2">
+                    <button className='bg-nig-60/20 rounded-3xl text-sm justify-center flex flex-col items-center px-3 py-3 h-[68px] w-[104px] border-[1px] border-b-0 border-r-0 border-service-0/10 text-nig-0 hover:bg-nig-60/50 transition-colors duration-300 ease-in-out hover:cursor-pointer'>
+                        <span className='font-semibold text-nig-40'>
+                            Active subs
+                        </span>
+                        <span className="text-md">12</span>
+                    </button>
+
+                    <button className='bg-nig-60/20 rounded-3xl text-sm justify-center flex flex-col items-center px-3 py-3 h-[68px] w-[104px] border-[1px] border-b-0 border-r-0 border-service-0/10 text-nig-0 hover:bg-nig-60/50 transition-colors duration-300 ease-in-out hover:cursor-pointer'>
+                        <span className='font-semibold text-nig-40'>
+                            Highest subs
+                        </span>
+                        <span className="text-md">$19.99</span>
+                    </button>
+
+                    <button className='bg-nig-60/20 rounded-3xl text-sm justify-center flex flex-col items-center px-3 py-3 h-[68px] w-[104px] border-[1px] border-b-0 border-r-0 border-service-0/10 text-nig-0 hover:bg-nig-60/50 transition-colors duration-300 ease-in-out hover:cursor-pointer'>
+                        <span className='font-semibold text-nig-40'>
+                            Lowest subs
+                        </span>
+                        <span className="text-md">$5.99</span>
+                    </button>
+                </div>
+            </div>
+
+            <div className="w-80 flex flex-col gap-3 font-semibold text-md">
+                <div className="bg-nig-100 h-[50px] rounded-2xl flex items-center px-3 justify-between">
+                    <button className='bg-nig-60/20 rounded-2xl justify-center flex items-center px-3 py-3 h-[36px] w-[155px] border-[1px] border-b-0 border-service-0/15 text-nig-0 hover:bg-nig-60/50 transition-colors duration-300 ease-in-out hover:cursor-pointer'>
+                        <span className='text-sm font-medium'>
+                            Your subscriptions
+                        </span>
+                    </button>
+                    <p className="text-sm px-6 text-nig-30 font-semibold">Upcoming bills</p>
+                </div>
+
+                <div className="flex justify-between rounded-3xl h-[64px] border-nig-70 border-[1px] items-center px-3">
+                    <div className="flex items-center gap-3">
+                        <div className="h-[40px] w-[40px] bg-[#1ED760] rounded-xl flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width={26} height={26} viewBox="0 0 24 24"><path fill="currentColor" d="M17.9 10.9C14.7 9 9.35 8.8 6.3 9.75c-.5.15-1-.15-1.15-.6c-.15-.5.15-1 .6-1.15c3.55-1.05 9.4-.85 13.1 1.35c.45.25.6.85.35 1.3c-.25.35-.85.5-1.3.25m-.1 2.8c-.25.35-.7.5-1.05.25c-2.7-1.65-6.8-2.15-9.95-1.15c-.4.1-.85-.1-.95-.5s.1-.85.5-.95c3.65-1.1 8.15-.55 11.25 1.35c.3.15.45.65.2 1m-1.2 2.75c-.2.3-.55.4-.85.2c-2.35-1.45-5.3-1.75-8.8-.95c-.35.1-.65-.15-.75-.45c-.1-.35.15-.65.45-.75c3.8-.85 7.1-.5 9.7 1.1c.35.15.4.55.25.85M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2"></path></svg>
+                        </div>
+                        <p>Spotify</p>
+                    </div>
+
+                    <p>$5.99</p>
+                </div>
+
+                <div className="flex justify-between rounded-3xl h-[64px] border-nig-70 border-[1px] items-center px-3">
+                    <div className="flex items-center gap-3">
+                        <div className="h-[40px] w-[40px] bg-[#FF0000] rounded-xl flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width={28} height={28} viewBox="0 0 24 24"><path fill="currentColor" d="m10 15l5.19-3L10 9zm11.56-7.83c.13.47.22 1.1.28 1.9c.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83c-.25.9-.83 1.48-1.73 1.73c-.47.13-1.33.22-2.65.28c-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44c-.9-.25-1.48-.83-1.73-1.73c-.13-.47-.22-1.1-.28-1.9c-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83c.25-.9.83-1.48 1.73-1.73c.47-.13 1.33-.22 2.65-.28c1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44c.9.25 1.48.83 1.73 1.73"></path></svg>
+                        </div>
+                        <p>Youtube Premium</p>
+                    </div>
+
+                    <p>$18.99</p>
+                </div>
+
+                <div className="flex justify-between rounded-3xl h-[64px] border-nig-70 border-[1px] items-center px-3">
+                    <div className="flex items-center gap-3">
+                        <div className="h-[40px] w-[40px] bg-[#ACDCF5] rounded-xl flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="p-[6px]" width={100} height={100} viewBox="0 0 256 165"><path fill="#0364b8" d="m154.66 110.682l52.842-50.534c-10.976-42.8-54.57-68.597-97.37-57.62a80 80 0 0 0-46.952 33.51c.817-.02 91.48 74.644 91.48 74.644"></path><path fill="#0078d4" d="m97.618 45.552l-.002.009a63.7 63.7 0 0 0-33.619-9.543c-.274 0-.544.017-.818.02C27.852 36.476-.432 65.47.005 100.798a63.97 63.97 0 0 0 11.493 35.798l79.165-9.915l60.694-48.94z"></path><path fill="#1490df" d="M207.502 60.148a53 53 0 0 0-3.51-.131a51.8 51.8 0 0 0-20.61 4.254l-.002-.005l-32.022 13.475l35.302 43.607l63.11 15.341c13.62-25.283 4.164-56.82-21.12-70.44a52 52 0 0 0-21.148-6.1"></path><path fill="#28a8ea" d="M11.498 136.596a63.91 63.91 0 0 0 52.5 27.417h139.994a51.99 51.99 0 0 0 45.778-27.323l-98.413-58.95z"></path></svg>
+                        </div>
+                        <p>Microsoft OneDrive</p>
+                    </div>
+
+                    <p>$29.99</p>
+                </div>
+            </div>
+
+            <div className="w-80 h-[64px] flex">
+                <button className='bg-nig-60/20 rounded-3xl rounded-r-none text-sm justify-center flex flex-col items-center px-3 py-3 border-[1px] border-b-0 border-r-0 border-service-0/10 text-nig-0 hover:bg-nig-60/50 transition-colors duration-300 ease-in-out hover:cursor-pointer'>
+                    <span className='font-semibold text-nig-40 px-2'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="M19.5 10a.5.5 0 0 0-1 0zm-14 0a.5.5 0 0 0-1 0zm15.146 2.354a.5.5 0 0 0 .708-.708zM12 3l.354-.354a.5.5 0 0 0-.708 0zm-9.354 8.646a.5.5 0 0 0 .708.708zM7 21.5h10v-1H7zM19.5 19v-9h-1v9zm-14 0v-9h-1v9zm15.854-7.354l-9-9l-.708.708l9 9zm-9.708-9l-9 9l.708.708l9-9zM17 21.5a2.5 2.5 0 0 0 2.5-2.5h-1a1.5 1.5 0 0 1-1.5 1.5zm-10-1A1.5 1.5 0 0 1 5.5 19h-1A2.5 2.5 0 0 0 7 21.5z"></path></svg>
+                    </span>
+                </button>
+                <button className='bg-nig-60/20 rounded-3xl rounded-l-none rounded-br-none border-l-0 text-sm justify-center flex flex-col items-center px-3 py-3 border-[1px] border-b-0 border-r-0 border-service-0/10 text-nig-0 hover:bg-nig-60/50 transition-colors duration-300 ease-in-out hover:cursor-pointer'>
+                    <span className='font-semibold text-nig-40 px-2'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"><circle cx={17} cy={7} r={3}></circle><circle cx={7} cy={17} r={3}></circle><path d="M14 14h6v5a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zM4 4h6v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1z"></path></g></svg>
+                    </span>
+                </button>
+
+                <button className='bg-peach-0 rounded-full px-3 py-3 h-12 mx-2 border-[1px] border-b-0 border-nig-0/50 text-nig-0 drop-shadow-text-shadow hover:bg-peach-50 transition-colors duration-300 ease-in-out hover:cursor-pointer'>
+                    <div className="absolute  top-0 left-0 bg-radial-[ellipse_105%_130%_at_50%_110%] from-transparent from-[43%] via-white/10 to-skin-0/50 h-12 rounded-3xl"></div>
+                    <span className='text-h2 font-medium'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-6-6h12"></path></svg>
+                    </span>
+                </button>
+
+                <button className='bg-nig-60/20 rounded-3xl rounded-r-none rounded-bl-none border-l-0 text-sm justify-center flex flex-col items-center px-3 py-3 border-[1px] border-b-0 border-r-0 border-service-0/10 text-nig-0 hover:bg-nig-60/50 transition-colors duration-300 ease-in-out hover:cursor-pointer'>
+                    <span className='font-semibold text-nig-40 px-2'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M4 4h16v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm0 4h16m-4-5v2M8 3v2"></path></svg>
+                    </span>
+                </button>
+                <button className='bg-nig-60/20 rounded-3xl rounded-l-none border-l-0 text-sm justify-center flex flex-col items-center px-3 py-3 border-[1px] border-b-0 border-r-0 border-service-0/10 text-nig-0 hover:bg-nig-60/50 transition-colors duration-300 ease-in-out hover:cursor-pointer'>
+                    <span className='font-semibold text-nig-40 px-2'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M3 9h18M3 5h18v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2zm4 8h3"></path></svg>
+                    </span>
+                </button>
+            </div>
+
+        </div>
+    )
+}
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -20,10 +20,10 @@ export default function Welcome() {
                 <div className='relative border-[0.5px] border-service-0/5 w-[75px] h-[75px] ml-[35%] mt-[5%] rounded-4xl rotate-[-24deg] bg-nig-60/20 border-r-0 border-b-0 shadow-md'>
                     <div className='border-[0.5px] border-service-0/5 w-[60px] h-[60px] rounded-3xl ml-[7px] mt-[7px] border-r-0 border-b-0 border-t-0 shadow-service-shadow inset-ring-black'></div>
 
-                    <div class="absolute w-[17px] h-[31] mx-auto bg-black left-[40%] top-[30%]">
-                        <div class="absolute top-0 left-0 w-[6px] h-[30px] bg-red-600 opacity-80"></div>
-                        <div class="absolute top-[-1px] left-[0.5px] w-[6px] h-[32px] bg-red-600 transform skew-y-[20deg] rotate-[-20deg] ml-[5px] z-10 shadow-md shadow-black"></div>
-                        <div class="absolute top-0 right-0 w-[6px] h-[30px] bg-red-600 opacity-80"></div>
+                    <div className="absolute w-[17px] h-[31] mx-auto bg-black left-[40%] top-[30%]">
+                        <div className="absolute top-0 left-0 w-[6px] h-[30px] bg-red-600 opacity-80"></div>
+                        <div className="absolute top-[-1px] left-[0.5px] w-[6px] h-[32px] bg-red-600 transform skew-y-[20deg] rotate-[-20deg] ml-[5px] z-10 shadow-md shadow-black"></div>
+                        <div className="absolute top-0 right-0 w-[6px] h-[30px] bg-red-600 opacity-80"></div>
                     </div>
                 </div>
 
@@ -39,7 +39,7 @@ export default function Welcome() {
                 <div className='relative border-[0.5px] border-service-0/5 w-[75px] h-[75px] mt-[5%] ml-[63%] rounded-4xl rotate-[24deg] bg-nig-60/20 border-r-0 border-b-0 scale-160 shadow-md'>
                     <div className='border-[0.5px] border-service-0/5 w-[60px] h-[60px] rounded-3xl ml-[7px] mt-[7px] border-r-0 border-b-0 border-t-0 shadow-service-shadow inset-ring-black'></div>
 
-                    {<div class="absolute left-[30%] top-[35%]">
+                    {<div className="absolute left-[30%] top-[35%]">
                         <svg xmlns="http://www.w3.org/2000/svg" width={30} viewBox="0 0 256 180"><path fill="#f00" d="M250.346 28.075A32.18 32.18 0 0 0 227.69 5.418C207.824 0 127.87 0 127.87 0S47.912.164 28.046 5.582A32.18 32.18 0 0 0 5.39 28.24c-6.009 35.298-8.34 89.084.165 122.97a32.18 32.18 0 0 0 22.656 22.657c19.866 5.418 99.822 5.418 99.822 5.418s79.955 0 99.82-5.418a32.18 32.18 0 0 0 22.657-22.657c6.338-35.348 8.291-89.1-.164-123.134"></path><path fill="#fff" d="m102.421 128.06l66.328-38.418l-66.328-38.418z"></path></svg>
                     </div>}
 
@@ -53,7 +53,7 @@ export default function Welcome() {
                 <div className='relative border-[0.5px] border-service-0/5 w-[75px] h-[75px] mt-[3%] ml-[7%] rounded-4xl rotate-[16deg] bg-nig-60/20 border-r-0 border-b-0 scale-130  shadow-md'>
                     <div className='border-[0.5px] border-service-0/5 w-[60px] h-[60px] rounded-3xl ml-[7px] mt-[7px] border-r-0 border-b-0 border-t-0 shadow-service-shadow inset-ring-black'></div>
 
-                    {<div class="absolute left-[25%] top-[30%]">
+                    {<div className="absolute left-[25%] top-[30%]">
                         <svg xmlns="http://www.w3.org/2000/svg" width={35} viewBox="0 0 256 165"><path fill="#0364b8" d="m154.66 110.682l52.842-50.534c-10.976-42.8-54.57-68.597-97.37-57.62a80 80 0 0 0-46.952 33.51c.817-.02 91.48 74.644 91.48 74.644"></path><path fill="#0078d4" d="m97.618 45.552l-.002.009a63.7 63.7 0 0 0-33.619-9.543c-.274 0-.544.017-.818.02C27.852 36.476-.432 65.47.005 100.798a63.97 63.97 0 0 0 11.493 35.798l79.165-9.915l60.694-48.94z"></path><path fill="#1490df" d="M207.502 60.148a53 53 0 0 0-3.51-.131a51.8 51.8 0 0 0-20.61 4.254l-.002-.005l-32.022 13.475l35.302 43.607l63.11 15.341c13.62-25.283 4.164-56.82-21.12-70.44a52 52 0 0 0-21.148-6.1"></path><path fill="#28a8ea" d="M11.498 136.596a63.91 63.91 0 0 0 52.5 27.417h139.994a51.99 51.99 0 0 0 45.778-27.323l-98.413-58.95z"></path></svg>
                     </div>}
 
@@ -67,7 +67,7 @@ export default function Welcome() {
                 <div className='relative border-[0.5px] border-service-0/5 w-[75px] h-[75px] mt-[10%] ml-[45%] rounded-4xl rotate-[-12deg] bg-nig-60/20 border-r-0 border-b-0 scale-200 shadow-md'>
                     <div className='border-[0.5px] border-service-0/5 w-[60px] h-[60px] rounded-3xl ml-[7px] mt-[7px] border-r-0 border-b-0 border-t-0 shadow-service-shadow inset-ring-black'></div>
 
-                    {<div class="absolute left-[27%] top-[24%]">
+                    {<div className="absolute left-[27%] top-[24%]">
                         <svg xmlns="http://www.w3.org/2000/svg" width={35} viewBox="0 0 256 256"><path fill="#1ed760" d="M128 0C57.308 0 0 57.309 0 128c0 70.696 57.309 128 128 128c70.697 0 128-57.304 128-128C256 57.314 198.697.007 127.998.007zm58.699 184.614c-2.293 3.76-7.215 4.952-10.975 2.644c-30.053-18.357-67.885-22.515-112.44-12.335a7.98 7.98 0 0 1-9.552-6.007a7.97 7.97 0 0 1 6-9.553c48.76-11.14 90.583-6.344 124.323 14.276c3.76 2.308 4.952 7.215 2.644 10.975m15.667-34.853c-2.89 4.695-9.034 6.178-13.726 3.289c-34.406-21.148-86.853-27.273-127.548-14.92c-5.278 1.594-10.852-1.38-12.454-6.649c-1.59-5.278 1.386-10.842 6.655-12.446c46.485-14.106 104.275-7.273 143.787 17.007c4.692 2.89 6.175 9.034 3.286 13.72zm1.345-36.293C162.457 88.964 94.394 86.71 55.007 98.666c-6.325 1.918-13.014-1.653-14.93-7.978c-1.917-6.328 1.65-13.012 7.98-14.935C93.27 62.027 168.434 64.68 215.929 92.876c5.702 3.376 7.566 10.724 4.188 16.405c-3.362 5.69-10.73 7.565-16.4 4.187z"></path></svg>
                     </div>}
 
