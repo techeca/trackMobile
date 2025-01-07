cc7f10263ff52a99107a96c25c24a0f9a8f88f49
techeca <jp.vasquez@outlook.com>
2025-01-07 18:39:06 -0300

    Change button for camponent Button.

--- src/components/pages/app/NewSubscription.jsx
+++ src/components/pages/app/NewSubscription.jsx
@@ -5,6 +5,7 @@ import DatePicker from '../../ui/DatePicker'
 const CALENDAR = 'calendar'
 import Input from '@components/ui/Input'
 import { useAppFn } from '@context/AppContext'
+import Button from '../../ui/Button'
 
 export default function NewSubscription() {
     const [price, setPrice] = useState(0)
@@ -32,19 +33,24 @@ export default function NewSubscription() {
 
     const handleSubmitNewSub = (e) => {
         e.preventDefault()
-        let newSubData = {
-            name: descValue,
-            price: price.toLocaleString('en-US'),
-            icon: selectedSub,
-            payDay: selectedDay,
-            payMonth: selectedMonth.slice(0, 3),
-            currency: "en-US",
-            description: descValue,
-            category: selectedCat,
-            reminder: "Never",
-            status: "Active",
+
+        try {
+            let newSubData = {
+                name: descValue,
+                price: price.toLocaleString('en-US'),
+                icon: selectedSub,
+                payDay: selectedDay,
+                payMonth: selectedMonth.slice(0, 3),
+                currency: "en-US",
+                description: descValue,
+                category: selectedCat,
+                reminder: "Never",
+                status: "Active",
+            }
+            subscription.add(newSubData)
+        } catch (error) {
+            console.log(error);
         }
-        subscription.add(newSubData)
     }
 
     return (
@@ -58,24 +64,24 @@ export default function NewSubscription() {
                 </div>
             </div>
 
-            <form onSubmit={handleSubmitNewSub} className="w-96 flex flex-col h-full gap-2 font-semibold text-md px-2 py-3 justify-start overflow-y-scroll border-0 border-white ">
+            <form onSubmit={handleSubmitNewSub} className="w-full flex flex-col text-center h-full gap-2 font-semibold text-md px-6 py-3 justify-start overflow-y-scroll border-0 border-white ">
 
-                <div className="w-80 flex flex-col ml-6 gap-1 text-nig-50 ">
+                <div className="w-full flex flex-col gap-1 text-nig-50">
                     <Input text='Description' name='description' value={descValue} onChange={(e) => setDescValue(e.target.value)} required />
                 </div>
 
                 <div className="text-nig-50">
-                    <label htmlFor="payday" className="text-sm text-start px-6 -mb-[20px]">Pay Day</label>
+                    <label htmlFor="payday" className="text-sm -mb-[20px]">Pay Day</label>
                     <DatePicker required selectedDay={selectedDay} selectedMonth={selectedMonth} setSelectedDay={setSelectedDay} setSelectedMonth={setSelectedMonth} />
                 </div>
 
-                <div className='flex w-full px-6 justify-between items-center py-3'>
+                <div className='flex w-full justify-between items-center py-3'>
                     <div onClick={() => handlePriceControl('minus', setPrice, price)} className='p-6 bg-nig-60/20 rounded-2xl border-l-[1px] border-service-0/15 h-[48px] w-[48px]'>
                         <svg xmlns="http://www.w3.org/2000/svg" className='text-nig-60 -ml-[23px] -mt-[23px]' width={46} height={46} viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M6 12h12"></path></svg>
                     </div>
                     <div className='text-xs font-semibold text-nig-40'>
                         <div className='flex flex-col items-center gap-1'>
-                            <p className='text-sm'>Monthly price</p>
+                            <p className='text-sm text-nig-50'>Monthly Price</p>
                             <input value={`${price}`} type='text' inputMode='decimal' min={0} onChange={(e) => handleInputChange(e.target.value, setPrice)} className='focus:outline-none border-b-[1px] border-nig-70 p-2 text-xl w-[162px] text-center text-white' />
                         </div>
                     </div>
@@ -96,12 +102,9 @@ export default function NewSubscription() {
 
                 {/*submit button*/}
                 <div className='w-full h-full flex flex-col justify-end items-center py-6'>
-                    <button className='bg-peach-0 rounded-3xl h-12 w-[85%] border-[1px] border-b-0 border-nig-0/50 text-nig-0 drop-shadow-text-shadow hover:bg-peach-50 transition-colors duration-300 ease-in-out hover:cursor-pointer'>
-                        <div className="absolute top-0 left-0 bg-radial-[ellipse_105%_130%_at_50%_110%] from-transparent from-[43%] via-white/10 to-skin-0/50 w-[85%] h-12 rounded-3xl"></div>
-                        <span className='text-h2 font-medium'>
-                            Add this platform
-                        </span>
-                    </button>
+                    <Button text='Add this platform' width={300} />
+
+                    
                 </div>
             </form>
 
